14b5456959271ee71034c76de3ff11eec777df07
João Soares <jsoaresgeral@gmail.com>
2013-03-26 01:35:34 +0000

    Bump version to v0.1.0
    Signed-off-by: João Soares <jsoaresgeral@gmail.com>

--- lib/travnoty/version.rb
+++ lib/travnoty/version.rb
@@ -1,8 +1,8 @@
 module Travnoty
   class Version
     MAJOR = 0 unless defined? Twitter::Version::MAJOR
-    MINOR = 0 unless defined? Twitter::Version::MINOR
-    PATCH = 1 unless defined? Twitter::Version::PATCH
+    MINOR = 1 unless defined? Twitter::Version::MINOR
+    PATCH = 0 unless defined? Twitter::Version::PATCH
 
     class << self
 
